3fb8b051187d88e732ec08bcea9b962376d75d8f
smarbaf <smarbaf@hotmail.com>
2015-05-12 12:03:13 +0100

    Added cucumber feature and first scenario

---
+++ features/steps_definitions/oh_hai_cucumner_steps.rb
@@ -0,0 +1,18 @@
+class LulzGreeter
+def greet
+    "OH HAI CUCUMNER!"
+  end
+end
+
+Given(/^a lulz greeter$/) do
+  @greeter = LulzGreeter.new
+end
+
+When(/^I send it the greet message$/) do
+  @message = @greeter.greet
+end
+
+Then(/^I should see "([^"]*)"$/) do |lulzy_greeting|
+  expect(@message).to eq lulzy_greeting
+end
+
